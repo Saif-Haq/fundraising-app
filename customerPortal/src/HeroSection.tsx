03e689ddb131105458cef7c79e44f2f35467740c
kwanso-saif <saif.ulhaq@kwanso.com>
2024-01-29 17:35:53 +0500

    Initialization

--- customerPortal/src/HeroSection.tsx
+++ customerPortal/src/HeroSection.tsx
@@ -1,10 +1,17 @@
-import './App.css'
-import './index.css'
+import "./App.css";
+import "./index.css";
 
-export const HeroSection = () =>
-    <div className="hero-container">
-        <div className='flex-space-between'>
-            <img src="../public/leader-alien.png" alt="Leader Alien Pic" />
-            <p>SAIF</p>
+export const HeroSection = () => (
+    <div className="sixteen-by-nine.aspect-ratio">
+        <div className="hero-container">
+            <div className="flex">
+                <div>
+                    <img src="../public/leader-alien.png" alt="Leader Alien Pic" />
+                </div>
+                <div>
+                    <p>SAIF</p>
+                </div>
+            </div>
         </div>
     </div>
+);
